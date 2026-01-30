7c603dc27201ab8f4a004ba4c2299733c5366699
Christopher Pepe <cpepe@tearsoffire.org>
2026-01-30 13:40:41 -0500

    all basic items defined. trader not impl

--- src/main/java/com/rainexael/fortnitefood/ModItems.java
+++ src/main/java/com/rainexael/fortnitefood/ModItems.java
@@ -48,9 +48,14 @@ public class ModItems {
 
                         .build();
 
-        public static final Consumable CHOCOLATE_BAR_CONSUMABLE_COMPONENT = Consumables.defaultFood()
+        public static final Consumable REGEN_2_CONSUMABLE_COMPONENT = Consumables.defaultFood()
                         .onConsume(new ApplyStatusEffectsConsumeEffect(
                                         new MobEffectInstance(MobEffects.REGENERATION, 100, 1), 1.0f))
+                        .build();
+
+        public static final Consumable COOKED_SMORE_CONSUMABLE_COMPONENT = Consumables.defaultFood()
+                        .onConsume(new ApplyStatusEffectsConsumeEffect(
+                                        new MobEffectInstance(MobEffects.HEALTH_BOOST, 1000000, 7), 1.0f))
                         .build();
 
         public static final Item SUSPICIOUS_SUBSTANCE = register("suspicious_substance", Item::new,
@@ -67,7 +72,18 @@ public class ModItems {
                         new Item.Properties().food(BANANA_FOOD_COMPONENT, BANANA_CONSUMABLE_COMPONENT));
         public static final Item CHOCOLATE_BAR = register("chocolate_bar", Item::new, new Item.Properties()
                         .food(new FoodProperties.Builder().nutrition(2).saturationModifier(0.3f).build(),
-                                        CHOCOLATE_BAR_CONSUMABLE_COMPONENT));
+                                        REGEN_2_CONSUMABLE_COMPONENT));
+        public static final Item CINNAMON = register("cinnamon", Item::new, new Item.Properties());
+        public static final Item GRAHAM_CRACKER = register("graham_cracker", Item::new, new Item.Properties()
+                        .food(new FoodProperties.Builder().nutrition(2).saturationModifier(0.3f).build(),
+                                        REGEN_2_CONSUMABLE_COMPONENT));
+        public static final Item MARSHMALLOW = register("marshmallow", Item::new, new Item.Properties()
+                        .food(new FoodProperties.Builder().nutrition(2).saturationModifier(0.3f).build(),
+                                        REGEN_2_CONSUMABLE_COMPONENT));
+        public static final Item RAW_SMORE = register("raw_smore", Item::new, new Item.Properties());
+        public static final Item COOKED_SMORE = register("cooked_smore", Item::new, new Item.Properties()
+                        .food(new FoodProperties.Builder().nutrition(8).saturationModifier(0.8f).build(),
+                                        COOKED_SMORE_CONSUMABLE_COMPONENT));
 
         public static Item register(String name, Function<Item.Properties, Item> itemFactory,
                         Item.Properties settings) {
@@ -101,5 +117,15 @@ public class ModItems {
                                 .register((itemGroup) -> itemGroup.accept(ModItems.BANANAOFTHEGODS));
                 ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FOOD_AND_DRINKS)
                                 .register((itemGroup) -> itemGroup.accept(ModItems.CHOCOLATE_BAR));
+                ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FOOD_AND_DRINKS)
+                                .register((itemGroup) -> itemGroup.accept(ModItems.CINNAMON));
+                ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FOOD_AND_DRINKS)
+                                .register((itemGroup) -> itemGroup.accept(ModItems.GRAHAM_CRACKER));
+                ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FOOD_AND_DRINKS)
+                                .register((itemGroup) -> itemGroup.accept(ModItems.MARSHMALLOW));
+                ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FOOD_AND_DRINKS)
+                                .register((itemGroup) -> itemGroup.accept(ModItems.RAW_SMORE));
+                ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FOOD_AND_DRINKS)
+                                .register((itemGroup) -> itemGroup.accept(ModItems.COOKED_SMORE));
         }
 }
